d97e457b6ee02a88bbbecc0568da2252c029cce1
sh3b0 <a.shaaban@innopolis.university>
2022-04-06 16:13:53 +0300

    Apply code inspection rules from eslint, prettier, webstorm

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -9,36 +17,36 @@ import { User } from './entities/user.entity';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @ApiOperation({summary: 'Create User/Sign up'})
-  @ApiResponse({status: 200, type: [User]})
+  @ApiOperation({ summary: 'Create User/Sign up' })
+  @ApiResponse({ status: 200, type: [User] })
   @Post()
   create(@Body() dto: CreateUserDto) {
     return this.usersService.create(dto);
   }
 
-  @ApiOperation({summary: 'Get all users'})
-  @ApiResponse({status: 200, type: [User]})
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, type: [User] })
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
-  @ApiOperation({summary: 'Get user by id'})
-  @ApiResponse({status: 200, type: [User]})
+  @ApiOperation({ summary: 'Get user by id' })
+  @ApiResponse({ status: 200, type: [User] })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @ApiOperation({summary: 'Update fields of a user'})
-  @ApiResponse({status: 200, type: [User]})
+  @ApiOperation({ summary: 'Update fields of a user' })
+  @ApiResponse({ status: 200, type: [User] })
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: CreateUserDto) {
     return this.usersService.update(+id, dto);
   }
 
-  @ApiOperation({summary: 'Delete user'})
-  @ApiResponse({status: 200, type: [User]})
+  @ApiOperation({ summary: 'Delete user' })
+  @ApiResponse({ status: 200, type: [User] })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
